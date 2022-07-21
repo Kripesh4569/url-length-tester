358c9cf908c431e51cc4c384d29595d39b701fc9
Kripesh4569 <kripsdhakal@gmail.com>
2022-07-21 18:48:30 +0200

    fix: server port for render (hardcoded)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,7 @@ use regex::Regex;
 use url_length_tester::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
